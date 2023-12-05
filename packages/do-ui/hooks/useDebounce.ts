648532a52afa76fb6b1ca294b110c0a15a7c18e6
efrice <719431749@qq.com>
2023-12-05 23:41:01 +0800

    feat: useDebounce hooks

---
+++ packages/do-ui/hooks/useDebounce.ts
@@ -0,0 +1,17 @@
+export type UseDebounceReturn = [() => void, () => void];
+
+export function useDebounce(
+  fn: Function,
+  ms = 300
+): UseDebounceReturn {
+  let timeout: number;
+
+  return [
+    () => {
+      clearTimeout(timeout);
+      timeout = setTimeout(fn, ms);
+    },
+    () => clearTimeout(timeout),
+  ];
+
+}
