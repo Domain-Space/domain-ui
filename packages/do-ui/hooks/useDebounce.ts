c59e6a52faefd381769695d00309b3efbdbfd48f
efrice <719431749@qq.com>
2024-01-09 21:56:25 +0800

    chore: useDebounce

--- packages/do-ui/hooks/useDebounce.ts
+++ packages/do-ui/hooks/useDebounce.ts
@@ -1,17 +1,13 @@
-export type UseDebounceReturn = [() => void, () => void];
+export type UseDebounceReturn = [() => void, () => void]
 
-export function useDebounce(
-  fn: Function,
-  ms = 300
-): UseDebounceReturn {
-  let timeout: number;
+export function useDebounce(fn: Function, ms = 300): UseDebounceReturn {
+  let timeout: number
 
   return [
     () => {
-      clearTimeout(timeout);
-      timeout = setTimeout(fn, ms);
+      clearTimeout(timeout)
+      timeout = setTimeout(fn, ms)
     },
-    () => clearTimeout(timeout),
-  ];
-
+    () => clearTimeout(timeout)
+  ]
 }
